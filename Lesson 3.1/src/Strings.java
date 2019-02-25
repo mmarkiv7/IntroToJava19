a3789ddf77b3d45587621a938f893150ede8ef3b
Vikram Sridhar <mmarkiv7@gmail.com>
2019-02-25 13:22:48 -0500

    commit 2/25

--- Lesson 3.1/src/Strings.java
+++ Lesson 3.1/src/Strings.java
@@ -2,7 +2,47 @@
 public class Strings {
 	
 	public static void main(String[] args){
-		
+	
+	//1
+	System.out.println("a\\b\\c\\\"d\"");
+	//2
+	System.out.println("\\t is a tab, \\n is a new line");
+	//3
+	System.out.println("\"line one\"\n\\line \t \\two\\");
+	//4
+	System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\*");
+	//5
+	System.out.println("\"\\\\\"\\\"\"\n\t\\\"\\\"\"\\\n\\\"\"\"\"\\");	
+	//6	
+	String bday = "Birthday";
+	System.out.println(bday.toUpperCase());
+	//7
+	String party = "it's my party";
+	String party2 = party.substring(0, 7);
+	String partySmall = party.substring(8, 13);
+	System.out.println(party2  + partySmall.toUpperCase());
+	//8
+	String whose = "whose a whats it";
+	//9
+	
+	//10
+	
+	//11
+	String subs = "if SUBSTRING is used";
+	
+	
+	//12
+	
+	//13
+	
+	
+	
+	
+	
+	
+	
+	//System.out.println(x.substring(2,6).toUpperCase());
+	
 		
 		
 		

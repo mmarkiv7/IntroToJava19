8be41aeeb1d725e88b055d6a289253e255f02ccb
Vikram Sridhar <mmarkiv7@gmail.com>
2019-02-28 13:58:37 -0500

    finished lesson 4.1 exercises

--- Lesson 4/src/NumericVariables.java
+++ Lesson 4/src/NumericVariables.java
@@ -3,46 +3,68 @@ public class NumericVariables {
 
 		public static void main (String []args){
 			
-			//1
+		//1
 			int h = 103;
 			int p = 5;
 			System.out.println(++h + p);
 			System.out.println(h);
-			//2
+		//2
 			int j = 1;
 			System.out.println(j + 1);
 			System.out.println(++j);
-			System.out.println(j +=1);
-			//4
+			System.out.println(j +=1); 
+		//3
 			double def;
-			double f = 1992.73;
+			double f = 1992.37;
 			def = f;
 			System.out.println(def);
-			//4
+		//4
 			int zulu = 20;
 			System.out.println(zulu - 1);
-			//5
+		//5
 			int a = 100;
 			int b = 200;
 			b/=a;
 			System.out.println(b+1);
-			//6
+		//6
 			int v = 60;
-			System.out.println(v-=30);
-			//7
-			System.out.println(v-30);
-			//8
+			v-=30;
+		//7
+			v = v-30;
+		//8
 			int q = 40;
 			int r = 4;
 			System.out.println(2+8 * q / 2 - p);
-			//9
+		//9
 			int sd = 12;
 			int x = 4;
 			System.out.println(sd%(++x));
 			System.out.println(x);
-			//10
+		//10
+			//would not compile
+		//11
+			double m11, b11, f11; m11 = b11 = f11 = 3.14;
+		//12
+			int x12, y12, z12;
+		//13
+			int m13 = 36;
+			int j13 = 5;
+			m13 = m13/j13;
+			System.out.println(m13);
+		//14
+			System.out.println(3/4 + 5*2/33 -3 +8*3);
+		//16
+			int i16 = 12;
+			int j16 = 2;
+			int k = i16/=j16;
+		//17
+			int j17=2;
+			System.out.println(7%3 + j17++ + (j17-2));
+		//18
 			
-			//11
+			
+			
+			
 			
 			
 

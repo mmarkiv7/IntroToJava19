f8a06f2bb344df7d2e09720092be4248f6c8d441
Vikram Sridhar <mmarkiv7@gmail.com>
2019-03-08 12:10:59 -0500

    lesson 6 commit 3/8

---
+++ Lesson 6/src/MathClass.java
@@ -0,0 +1,21 @@
+
+public class MathClass {
+	
+	public static void main(String []args){
+		double x = 1;
+		double y = Math.sqrt(x);
+		
+		int j = 2;
+		int m = -1;
+		int k = (j * Math.abs(m));
+		
+		double p =1;
+		double b = (p* Math.PI);
+		
+		double a = 18;
+		System.out.println(Math.log(18));
+		
+		
+	}
+
+}
---
+++ Lesson 6/src/Project.java
@@ -0,0 +1,29 @@
+
+public class Project {
+
+	public static void main(String []args){
+		
+		
+		
+		double d1;
+		
+		double cos1 = Math.cos(2.1293);
+		
+		
+		d1 = 3 * Math.PI * Math.sin(3.2637659) + Math.abs(cos1);
+		
+		double p1 = 14.72;
+		double p2 = 3.801;
+		double a = 72;
+		
+		double d2;
+		d2 = Math.pow(p1, p2) + Math.log(a);
+		
+		System.out.println("d1 = " + d1);
+		System.out.println("d2 = " + d2);
+	
+	
+	}
+	
+
+}

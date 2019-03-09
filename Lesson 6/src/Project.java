7875ae1062f08a1670c50b2848bff113eb962e7e
Vikram Sridhar <vikram.sridhar@motsd.org>
2019-03-09 18:20:28 -0500

    finished lesson 6 project

--- Lesson 6/src/Project.java
+++ Lesson 6/src/Project.java
@@ -7,10 +7,10 @@ public class Project {
 		
 		double d1;
 		
-		double cos1 = Math.cos(2.1293);
+		double cos1 = Math.cos(2.12930168743);
 		
 		
-		d1 = 3 * Math.PI * Math.sin(3.2637659) + Math.abs(cos1);
+		d1 = 3 * Math.PI * Math.sin(3.26376570123) + Math.abs(cos1);
 		
 		double p1 = 14.72;
 		double p2 = 3.801;

b4a6cbc18b005d4cedca36ceb7bc3c9c1e5e1863
Vikram Sridhar <mmarkiv7@gmail.com>
2019-03-12 13:55:58 -0400

    lolz

--- Lesson 6/src/Project.java
+++ Lesson 6/src/Project.java
@@ -1,29 +1,24 @@
 
 public class Project {
 
-	public static void main(String []args){
-		
-		
-		
+	public static void main(String[] args) {
+
 		double d1;
-		
-		double cos1 = Math.cos(2.12930168743);
-		
-		
-		d1 = 3 * Math.PI * Math.sin(3.26376570123) + Math.abs(cos1);
-		
+
+	
+
+		d1 = 3 * Math.PI * Math.sin(Math.toRadians(187)) + Math.abs(Math.cos(Math.toRadians(122)));
+
 		double p1 = 14.72;
 		double p2 = 3.801;
 		double a = 72;
-		
+
 		double d2;
 		d2 = Math.pow(p1, p2) + Math.log(a);
-		
+
 		System.out.println("d1 = " + d1);
 		System.out.println("d2 = " + d2);
-	
-	
+
 	}
-	
 
 }

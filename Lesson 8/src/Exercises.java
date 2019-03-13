30c82ef4c975898cae9a8375a9cd81363d87e3ef
Vikram Sridhar <mmarkiv7@gmail.com>
2019-03-13 13:18:33 -0400

    3/13

--- Lesson 8/src/Exercises.java
+++ Lesson 8/src/Exercises.java
@@ -7,8 +7,20 @@ public class Exercises {
 	
 		Scanner scanner = new Scanner(System.in);
 		
-		boolean a = (2 == 3);
-		System.out.println(a);
+		int z = 23, x = -109;
+		double c = 2345.19, v = 157.03;
+		boolean a = false, s = true;
+	
+		
+		
+		boolean gus = (x>0) && (c==v);
+		boolean r = z ==x;
+		System.out.println(z!=x);
+		
+		int a1 = 2;
+		int b2 = 3;
+		boolean kDog = !(a1==b2);
+		
 		
 		
 			
